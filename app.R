44c9e63fd5f88ef484d9e470c07737fed16e0be3
Rod Miller <92232804+RodNSS@users.noreply.github.com>
2023-11-03 22:05:10 -0500

    Added highcharter interactivity

--- app.R
+++ app.R
@@ -5,52 +5,72 @@ library(osmdata)
 library(htmlwidgets)
 library(tidygeocoder)
 library(leaflet.extras)
-library(ggplot2)
 library(glue)
 library(shiny)
-library(bslib)
-library(thematic)
 library(htmltools)
-thematic::thematic_shiny(font = "auto")
+library(highcharter)
 
+# Load Mapbox API key and map attribution
 mapbox <- Sys.getenv("MAPBOX_API_KEY")
 attribution <- "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a"
+
 # Function to retrieve and process data
 get_data <- function(data_source) {
+  
   # Retrieve data from the API
   data_url <- data_source
   data <- fromJSON(data_url)
   
-  # Add Nashville to the address column for geocoding coordinates
+  # Replace '/' with '&' and add Nashville, Brentwood or Hermitage to the address column for better geocoding accuracy
   data <- data %>%
-    mutate(address = gsub("/", "&", address)) %>%
-    mutate(address = ifelse(grepl("OLD HICKORY BLVD", address) & city == "HERMITAGE",
-                            paste0(address, ' ', "Hermitage, TN"),
-                            paste0(address, ' ', "Nashville, TN")))
+    mutate(
+      address = gsub("/", "&", address),
+      address = ifelse(
+        grepl("OLD HICKORY BLVD", address) & city == "BRENTWOOD DAVIDSON COUNTY",
+        paste0(address, ' ', "Brentwood, TN"),
+        ifelse(
+          grepl("OLD HICKORY BLVD", address) & city == "HERMITAGE",
+          paste0(address, ' ', "Hermitage, TN"),
+          paste0(address, ' ', "Nashville, TN")
+        )
+      )
+    )
   
-  # Get lat_long coordinates
-  lat_long <- geo(address = data$address, method = "arcgis")
-  # Process the data
+  # Get lat/long coordinates
+  lat_long <- geo(address = data$address, method = "arcgis", full_results = TRUE)
+  
+  # Mutate the data
   data <- data %>% 
     select(-address) %>% 
     cbind(lat_long) %>% 
-    mutate(address = gsub("Nashville, TN", "", address),
-           coord = paste(lat, long, sep = ","),  # Creates new column with the latitude and longitude coordinates
-           gsv = glue("http://maps.google.com/maps?q=&layer=c&cbll={coord}"),  # Creates new column with the Google Street View URL
-           gsv_links = paste0('<a target=_blank href=', gsv, '>Street View</a>'),  # Creates new column with the Google Street View clickable link
-           incident_type = gsub("ROBERRY/HOLD UP ALARM", "ROBBERY/HOLD UP ALARM", incident_type)) # Robbery is misspelled in the json data
+    mutate(
+      address = arcgis_address,
+      full_address = gsub(" ", "%20", arcgis_address),  # Format address for Google Maps link
+      coord = paste(lat, long, sep = ","),  # Create new column with coordinates
+      
+      # Use conditional to distinguish between cross street coordinates and actual addresses
+      gsv = ifelse(
+        grepl("&", arcgis_address),
+        glue("http://maps.google.com/maps?q=&layer=c&cbll={coord}"),
+        glue("http://maps.google.com/maps?q={full_address}")
+      ),
+      
+      gsv_links = paste0('<a target="_blank" href=', gsv, '>Street View</a>'), # Google Street View links
+      incident_type = gsub("ROBERRY/HOLD UP ALARM", "ROBBERY/HOLD UP ALARM", incident_type)  # Correct spelling error 
+    )
   
   df <- data %>% 
     select(incident_type, call_received, address, city, lat, long, gsv_links) %>% 
-    mutate(call_received = format(as.POSIXct(call_received, format = "%Y-%m-%dT%H:%M:%S"), "%I:%M:%S %p"), # Converts military time
-           city = as.character(city)) 
+    mutate(
+      call_received = format(as.POSIXct(call_received, format = "%Y-%m-%dT%H:%M:%S"), "%I:%M:%S %p"),  # Convert military time
+      city = as.character(city)
+    )
   
   return(df)
 }
-theme <- bslib::bs_theme(version = 5, bootswatch = "superhero", bg = '#222222', fg = '#11F5DF' )
-# Shiny app code
+
+# UI setup
 ui <- fluidPage(
-  theme = theme,
   tags$style(HTML("
     #map { 
       position: absolute; 
@@ -60,28 +80,37 @@ ui <- fluidPage(
       right: 0; 
     }
     #chart {
-    position: absolute;
-    bottom: 10px;
-    left: 10px;
-    z-index: 999;
-    background-color: rgb(34, 34, 34); 
-    opacity: 0.8;
-    color: rgb(105, 255, 255); 
+      position: absolute;
+      bottom: 10px;
+      left: 10px;
+      z-index: 999;
+      opacity: 0.8;
     }
-    #chart .plot-container { 
-    background-color: rgb(105, 255, 255);
+    #github-link {
+      position: absolute;
+      top: 10px;
+      right: 10px;
+      z-index: 1;  
+      font-size: 36px;  
+      color: #11F5DF;
     }
-    ")),
+  ")),
   leafletOutput("map", width = "100%", height = "100%"),
-  plotOutput("chart", height = 250, width = 500)
+  highchartOutput("chart", height = 300, width = 500), 
+  
+  # GitHub link with icon
+  tags$a(href = "https://github.com/RodNSS/Nashville_Active_Incident_Map", 
+         target = "_blank", 
+         icon("github", lib = "font-awesome"),
+         id = "github-link")
 )
 
 server <- function(input, output, session) {
-  #bs_themer()
-  # Define variable to hold leaflet map
-  m <- leaflet() %>% 
-    addTiles(mapbox, attribution) %>% 
-    setView(lat = 36.1627, lng = -86.7816, zoom = 11) %>% 
+  
+  # Define variable to hold Leaflet basemap
+  m <- leaflet() %>%
+    addTiles(mapbox, attribution) %>%
+    setView(lat = 36.1627, lng = -86.7816, zoom = 11) %>%
     addResetMapButton()
   
   # Update data source every 15 minutes
@@ -110,33 +139,48 @@ server <- function(input, output, session) {
                         "<br>", "Address: ", address, 
                         "<br>", "City/Area: ", city,
                         "<br>", gsv_links), 
-        clusterOptions = markerClusterOptions(spiderfyDistanceMultiplier=1.5, maxClusterRadius = 1),
+        clusterOptions = markerClusterOptions(
+          spiderfyDistanceMultiplier = 1.5,
+          maxClusterRadius = 1,
+          iconCreateFunction = JS(
+            "function(cluster) {
+              return L.divIcon({ 
+                html: '<div style=\"font-size: 16px; color: #000;\">' + cluster.getChildCount() + '</div>',
+                className: 'custom-cluster-icon',
+                iconSize: L.point(20, 20)
+              });
+            }"
+          )
+        ),
         icon = makePulseIcon(
           heartbeat = ifelse(data_source()$incident_type %in% c("SHOTS FIRED", 
                                                                 "SHOOTING", 
                                                                 "HOLD UP ROBBERY IN PROGRESS", 
-                                                                "ROBBERY/HOLD UP ALARM", 
+                                                                "ROBBERY/HOLD UP ALARM",
+                                                                "CUTTING OR STABBING IN PROGRESS",
+                                                                "CUTTING OR STABBING IN PROGRESS JUVENILE",
                                                                 "HOLD UP ROBBERY IN PROGRESS JUVENILE",
                                                                 "SHOOTING IN PROGRESS JUVENILE"), 1, 2), 
           iconSize = c(10, 10), 
           color = ifelse(data_source()$incident_type %in% c("SHOTS FIRED", 
                                                             "SHOOTING", 
                                                             "HOLD UP ROBBERY IN PROGRESS", 
-                                                            "ROBBERY/HOLD UP ALARM", 
+                                                            "ROBBERY/HOLD UP ALARM",
+                                                            "CUTTING OR STABBING IN PROGRESS",
+                                                            "CUTTING OR STABBING IN PROGRESS JUVENILE",
                                                             "HOLD UP ROBBERY IN PROGRESS JUVENILE",
                                                             "SHOOTING IN PROGRESS JUVENILE"), "red", "orange")
         )
       )
   })
   
-  
   # Update markers every 15 minutes
   observeEvent(data_source(), {
     leafletProxy("map") %>%
       clearMarkers() %>%
       addPulseMarkers(
-        data = data_source(),
-        lat = ~lat,
+        data = data_source(), 
+        lat = ~lat, 
         lng = ~long,
         label = ~paste0("Incident Type: ", incident_type, 
                         "<br>", "Call Received: ", call_received, 
@@ -146,51 +190,146 @@ server <- function(input, output, session) {
                         "<br>", "Call Received: ", call_received, 
                         "<br>", "Address: ", address, 
                         "<br>", "City/Area: ", city,
-                        "<br>", gsv_links),
-        clusterOptions = markerClusterOptions(spiderfyDistanceMultiplier = 1.5, maxClusterRadius = 1),
+                        "<br>", gsv_links), 
+        clusterOptions = markerClusterOptions(
+          spiderfyDistanceMultiplier = 1.5,
+          maxClusterRadius = 1,
+          iconCreateFunction = JS(
+            "function(cluster) {
+              return L.divIcon({ 
+                html: '<div style=\"font-size: 16px; color: #000;\">' + cluster.getChildCount() + '</div>',
+                className: 'custom-cluster-icon',
+                iconSize: L.point(20, 20)
+              });
+            }"
+          )
+        ),
         icon = makePulseIcon(
           heartbeat = ifelse(data_source()$incident_type %in% c("SHOTS FIRED", 
                                                                 "SHOOTING", 
                                                                 "HOLD UP ROBBERY IN PROGRESS", 
-                                                                "ROBBERY/HOLD UP ALARM", 
+                                                                "ROBBERY/HOLD UP ALARM",
+                                                                "CUTTING OR STABBING IN PROGRESS",
+                                                                "CUTTING OR STABBING IN PROGRESS JUVENILE",
                                                                 "HOLD UP ROBBERY IN PROGRESS JUVENILE",
                                                                 "SHOOTING IN PROGRESS JUVENILE"), 1, 2), 
           iconSize = c(10, 10), 
           color = ifelse(data_source()$incident_type %in% c("SHOTS FIRED", 
                                                             "SHOOTING", 
                                                             "HOLD UP ROBBERY IN PROGRESS", 
-                                                            "ROBBERY/HOLD UP ALARM", 
+                                                            "ROBBERY/HOLD UP ALARM",
+                                                            "CUTTING OR STABBING IN PROGRESS",
+                                                            "CUTTING OR STABBING IN PROGRESS JUVENILE",
                                                             "HOLD UP ROBBERY IN PROGRESS JUVENILE",
                                                             "SHOOTING IN PROGRESS JUVENILE"), "red", "orange")
         )
       )
   })
-  
-  
-  # Bar chart output
-  output$chart <- renderPlot({
+  # Highcharter code
+  output$chart <- renderHighchart({
     crime_count <- data_source() %>%
       group_by(incident_type) %>%
       summarize(count = n()) %>%
       arrange(desc(count))
     
-    ggplot(crime_count, aes(x = reorder(incident_type, count), y = count)) +
-      geom_bar(stat = "identity") +
-      coord_flip() +
-      xlab("") +
-      ylab("") +
-      ggtitle(paste(nrow(data_source()),"ACTIVE INCIDENTS")) +
-      geom_text(aes(y = count, label = count), hjust = -0.2, size = 5) +
-      theme(legend.position = "none",
-            panel.background = element_rect(fill = "#222222"), 
-            panel.grid.major = element_blank(), 
-            panel.grid.minor = element_blank(), 
-            axis.text.x = element_blank(),
-            axis.ticks.x = element_blank(),
-            axis.text.y = element_text(size = 12),
-            plot.title = element_text(size = 13)) 
+    # Check if there's only one incident_type so chart displays properly
+    if (nrow(crime_count) == 1) {
+      crime_count <- tibble(incident_type = crime_count$incident_type, count = crime_count$count)
+    }
+    
+    # Create the chart and interactive features
+    hc <- highchart() %>%
+      hc_chart(type = "bar") %>%
+      hc_tooltip(enabled = FALSE
+      ) %>%
+      hc_title(
+        text = paste(nrow(data_source()), "ACTIVE INCIDENTS"),
+        style = list(fontSize = "15px", fontWeight = "bold", color = '#11F5DF')
+      ) %>%
+      hc_xAxis(
+        categories = crime_count$incident_type,
+        labels = list(style = list(color = '#11F5DF', fontSize = "13px")),
+        lineWidth = 0,  
+        lineColor = "transparent",
+        tickmarkPlacement = "on",
+        tickColor = '#11F5DF',
+        tickWidth = 1
+      ) %>%
+      hc_yAxis(
+        labels = list(enabled = FALSE),
+        gridLineWidth = 0,
+        gridLineColor = "transparent"
+      ) %>% 
+      hc_add_series(
+        data = crime_count$count,
+        name = "Incident Count",
+        showInLegend = FALSE,
+        cursor = "pointer",
+        color = '#11F5DF',
+        point = list(
+          events = list(
+            mouseOver = JS("
+            function () {
+              Shiny.setInputValue('selectedIncident', this.category);
+              this.graphic.attr({
+                fill: '#FF00FF'  // Changes the color when hovering over the bar
+              });
+            }
+          "),
+            mouseOut = JS("
+            function () {
+              Shiny.setInputValue('selectedIncident', null);
+              var originalColor = this.color;  // Store the original color
+              this.graphic.attr({
+                fill: this.color  // Revert to original color on mouseOut
+              });
+            }
+          ")
+          )
+        ) 
+      ) %>%
+      hc_plotOptions(
+        bar = list(groupPadding = 0, pointWidth = 25, borderRadius = 9,
+                   dataLabels = list(
+                     enabled = TRUE,
+                     color = '#11F5DF',
+                     style = list(fontSize = '14px', 
+                                  fontWeight = 'normal',
+                                  textOutline = 'transparent'
+                     )
+                   ),
+                   borderWidth = 0
+        )
+      )
+    
+    hc
   })
+  # Observe the selected incident and add/clear markers
+  observe({
+    if (!is.null(input$selectedIncident)) {
+      # Handle the selected incident
+      selected_incident <- input$selectedIncident
+      markers_to_show <- data_source() %>%
+        filter(incident_type == selected_incident)
+      
+      # Add markers based on bar hover
+      leafletProxy("map") %>%
+        clearMarkers() %>%
+        addCircleMarkers(
+          data = markers_to_show,
+          lat = ~lat,
+          lng = ~long,
+          color = '#FF00FF', 
+          radius = 15,  
+          fillOpacity = 0.1,  
+          opacity = 1 
+        )
+    } else {
+      leafletProxy("map") %>%
+        clearMarkers()
+    }
+  })
+  
 }
-
 
 shinyApp(ui = ui, server = server)
